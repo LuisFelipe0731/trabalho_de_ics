fc2de6302e3b68d344edcffb6b7921345aa1b723
Luis Felipe <161077479+LuisFelipe0731@users.noreply.github.com>
2026-02-06 14:12:29 -0300

    Update remove.php

--- site-backend/autores/remove.php
+++ site-backend/autores/remove.php
@@ -1,1 +1,20 @@
 
+<html>
+	<?php
+	include('connection.php');
+	?>  
+
+	<body>
+
+	<?php
+	$query = 'DELETE FROM autores WHERE id = '.$_GET['id'];
+	$result = mysqli_query($db, $query) or die(mysqli_error($db));
+	?>
+		
+	<script type="text/javascript">
+		alert("Autor excluído com sucesso.");
+		window.location = "index.php";
+	</script>				
+				
+	</body>
+</html>
