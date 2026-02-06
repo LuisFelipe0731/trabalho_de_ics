685ef75332a6a62a2e5335da4a3177e0935b71e2
Luis Felipe <161077479+LuisFelipe0731@users.noreply.github.com>
2026-02-06 14:03:13 -0300

    Update add_post.php

--- site-backend/autores/add_post.php
+++ site-backend/autores/add_post.php
@@ -1,26 +1,27 @@
 <html>
 <?php
-    include('connection.php');
+	include('connection.php');
 ?>   
 
 <body>
 
-<?php
-    $nome = $_POST['nome'];
-    $nacionalidade = $_POST['nacionalidade'];
+	<?php
+	$nome = $_POST['nome'];
+	$nacionalidade = $_POST['nacionalidade'];
 
-    $query = "INSERT INTO autores (id, nome, nacionalidade)
-              VALUES (NULL, '$nome', '$nacionalidade')";
+	$query = "INSERT INTO autores
+			(id, nome, nacionalidade)
+			VALUES (NULL, '".$nome."', '".$nacionalidade."')";
 
-    mysqli_query($db, $query) 
-        or die('Error in Database: ' . mysqli_error($db));
-?>
-    
-<script type="text/javascript">
-    alert("Autor adicionado com sucesso.");
-    window.location = "index.php";
-</script>
+	mysqli_query($db, $query) or die('Error in Database: '.mysqli_error($db));
+	?>
+	
+	<script type="text/javascript">
+		alert("Autor cadastrado com sucesso.");
+		window.location = "index.php";
+	</script>
 
 </body>
 </html>
+
 
